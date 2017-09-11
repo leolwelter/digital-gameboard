2b90377b908c295c712e334a1b8d6ac34abe96cb
Station05 <Station05@EE07PC105.ecn.purdue.edu>
2017-09-11 12:10:47 -0400

    initialize embedded branch

---
+++ main.c
@@ -0,0 +1,23 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  Ac6
+  * @version V1.0
+  * @date    01-December-2013
+  * @brief   Default main function.
+  ******************************************************************************
+*/
+
+
+#include "stm32f7xx.h"
+#include "stm32f7xx_nucleo_144.h"
+			
+
+int main(void)
+{
+	/*BSP_LED_Init(LED3);
+	for(;;)
+	{
+		BSP_LED_Toggle(LED3);
+	}*/
+}

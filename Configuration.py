543f8fd8ad77d1474d8b2216c1d641a3309e3db2
jad36 <durbinj@purdue.edu>
2017-11-06 12:40:22 -0500

    changed some gui stuff, map (testmap) loads from firebase

---
+++ Configuration.py
@@ -0,0 +1,10 @@
+def getConfig():
+    config = {
+        "apiKey": "AIzaSyDRVUzSZjcBev8uY0eiL3zwqwy7MIVpIFc",
+        "authDomain": "tabletop-1c4a9.firebaseapp.com",
+        "databaseURL": "https://tabletop-1c4a9.firebaseio.com",
+        "projectID": "tabletop-1c4a9",
+        "storageBucket": "tabletop-1c4a9.appspot.com",
+        "messagingSenderId": "875322092974"
+    }
+    return config
